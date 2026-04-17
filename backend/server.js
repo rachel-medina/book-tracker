98e9e6fcd7f976c3b8f3ee52e19da4007fd0a33d
Rachel Medina <rmedina6118@sdsu.edu>
2026-04-16 23:26:56 -0700

    add basic books API with GET and POST routes

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,28 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+
+let books = []; // temp storage
+
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("API is running");
+    res.send("API is running");
+});
+
+// GET all books
+app.get("/books", (req, res) => {
+    res.json(books);
+});
+
+// ADD a book
+app.post("/books", (req, res) => {
+    const newBook = req.body;
+
+    books.push(newBook);
+
+    res.json(newBook);
 });
 
 const PORT = 5000;
